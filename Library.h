a06e327edacc66520960c1e6325624b1ec18f256
tamas <tamas@dElite>
2024-07-17 10:25:26 +0200

    takePhotoSend

--- Library.h
+++ Library.h
@@ -3,6 +3,7 @@
 #include "soc/soc.h"            //disable brownout problems
 #include "soc/rtc_cntl_reg.h"   //disable brownout problems
 #include "driver/rtc_io.h"
+#include <esp_camera.h>
 
 #include <SPI.h>
 #include <SX127XLT.h>           //get library here > https://github.com/StuartsProjects/SX12XX-LoRa
@@ -10,50 +11,39 @@
 
 SX127XLT LoRa;          //create a library class instance called LoRa, needed for ARtransferIRQ.h
 
-#define SD SD_MMC
 //#include "SDlibrary.h"
+#include "Settings.h"   
+#include <ARtransferIRQ.h>   //library of transfer functions
+#include "DTSDlibrary.h"
 
-#define ENABLEMONITOR   //enable define to see progress messages in ARtransferIRQ.h
-#define PRINTSEGMENTNUM //enable to print segment numbers as transfer progresses
-#define ENABLEARRAYCRC  //enable this define to use and show CRCs
-//#define DISABLEPAYLOADCRC //enable this define if you want to not use packet payload CRC checking                                                              
-#define DEBUG           //enable this define to show data transfer debug info                                                                                  
-RTC_DATA_ATTR int16_t bootCount = 0;             //variables to save in RTC ram
-RTC_DATA_ATTR uint16_t sleepcount = 0;
+#include "esp_camera.h"
+char DTfilenamebuff[] = "/$50SATS.JPG"; //file length 6880 bytes, file CRC 0x0281
 
-#include <esp_camera.h>
-camera_config_t config; //stores the camera configuration parameters
-#include "ARtransfer.h"   //library of transfer functions
+uint8_t *ptrDTsendarray;                //create a global pointer to the array to send, so all functions have access
 
-uint8_t *PSRAMptr;      //create a global pointer to the array to send, so all functions have access
-uint32_t available_PSRAM;
-uint32_t allocated_PSRAM;
-
-#define NSS 12         //select on LoRa device
-#define NRESET 15      //reset pin on LoRa device
-#define SCK 14         //SCK on SPI3
-#define MISO 13        //MISO on SPI3
-#define MOSI 2         //MOSI on SPI3
-#define DIO0 4         //DIO0 on SPI3
-
-#define REDLED 33      //pin number for ESP32CAM on board red LED, set logic level low for on
-#define WHITELED 4     //pin number for ESP32CAM on board white LED
-
-#define LORA_DEVICE DEVICE_SX1278               //this is the device we are using
-#define Monitorport Serial                      //Serial port for prints
-
+void redFlash(uint16_t flashes, uint16_t ondelaymS, uint16_t offdelaymS);
 void boot();
 bool setupLoRaDevice();
+bool configInitCamera();
 bool WaitForArray();
-void redFlash(uint16_t flashes, uint16_t ondelaymS, uint16_t offdelaymS);
+bool sendArray(uint8_t *array, uint32_t arrayL);
+bool takePhotoSend();
 
-//*******  Setup LoRa modem parameters here ! ***************
-const uint32_t Frequency = 434000000;           //frequency of transmissions in hertz
-const uint32_t Offset = 0;                      //offset frequency for calibration purposes
-const uint8_t Bandwidth = LORA_BW_500;          //LoRa bandwidth
-const uint8_t SpreadingFactor = LORA_SF7;       //LoRa spreading factor
-const uint8_t CodeRate = LORA_CR_4_5;           //LoRa coding rate
-const uint8_t Optimisation = LDRO_AUTO;         //low data rate optimisation setting, normally set to auto                                                                            
+void redFlash(uint16_t flashes, uint16_t ondelaymS, uint16_t offdelaymS)
+{
+  uint16_t index;
+
+  pinMode(REDLED, OUTPUT);                    //setup pin as output
+
+  for (index = 1; index <= flashes; index++)
+  {
+    digitalWrite(REDLED, LOW);
+    delay(ondelaymS);
+    digitalWrite(REDLED, HIGH);
+    delay(offdelaymS);
+  }
+  pinMode(REDLED, INPUT);                     //setup pin as input
+}
 
 void boot()
 {
@@ -117,6 +107,12 @@ void boot()
 
   uint8_t *byte_array = (uint8_t *) ps_malloc((allocated_PSRAM) * sizeof(uint8_t));
   PSRAMptr = byte_array;                                 //save the pointer to byte_array as global pointer
+
+  if (!configInitCamera())
+  {
+    Monitorport.println(F("Camera config failed"));
+    while (1) redFlash(100, 25, 25);
+  }
 }
 
 bool setupLoRaDevice()
@@ -149,50 +145,273 @@ bool setupLoRaDevice()
   return true;
 }
 
+bool configInitCamera()
+{
+  Serial.println(F("Initialising the camera module "));
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;      //YUV422,GRAYSCALE,RGB565,JPEG
+
+  //Select lower framesize if the camera doesn't support PSRAM
+  if (psramFound())
+  {
+    Serial.println(F("PSRAM found"));
+    config.frame_size = FRAMESIZE_UXGA;      //FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 2;                 //0-63 lower number means higher quality
+    config.fb_count = 2;
+
+    // approximate file sizes
+    // @ jpeg_quality = 10, SVGA image size = 25K+, UXGA image size = 55K+,
+    // @ jpeg_quality = 2, SVGA image size = 60K+, UXGA image size = 160K+,                   // image sizes of circa 200K+ can cause ESP32CAM camera code to crash
+  }
+  else
+  {
+    Serial.println(F("No PSRAM"));
+    config.frame_size = FRAMESIZE_XGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  esp_err_t err = esp_camera_init(&config);   //Initialize the Camera
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    Serial.println();
+    return false;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_brightness(s, 0);     // -2 to 2
+  s->set_contrast(s, 0);       // -2 to 2
+  s->set_saturation(s, 0);     // -2 to 2
+  s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+  s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+  s->set_wb_mode(s, 1);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  s->set_aec2(s, 1);           // 0 = disable , 1 = enable
+  s->set_ae_level(s, 0);       // -2 to 2
+  s->set_aec_value(s, 450);    // 0 to 1200
+  s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+  s->set_agc_gain(s, 0);       // 0 to 30
+  s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+  s->set_bpc(s, 1);            // 0 = disable , 1 = enable
+  s->set_wpc(s, 0);            // 0 = disable , 1 = enable
+  s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+  s->set_lenc(s, 0);           // 0 = disable , 1 = enable
+  s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+  s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+  s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+  s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
+  return true;
+}
+
 bool WaitForArray()
 {
   setupLoRaDevice();
   Monitorport.println(F("LoRa file transfer receiver ready"));
 
-//  DTreceiveArrayL = ARreceiveArray(PSRAMptr, allocated_PSRAM, ReceiveTimeoutmS);
+  ARreceiveArrayL = ARreceiveArrayNoACK(PSRAMptr, allocated_PSRAM, ReceiveTimeoutmS);
 
   SPI.end();
   digitalWrite(NSS, HIGH);
   digitalWrite(NRESET, HIGH);
   delay(500);
 
-//  if (DTreceiveArrayL)
-//  {
-//    Monitorport.print(F("Received array length "));
-//    Monitorport.println(DTreceiveArrayL);
-//    return true;
-//  }
-//  else
-//  {
-//    Monitorport.println(F("Error receiving array"));
-//    if (ARDTArrayTimeout)
-//    {
-//      Monitorport.println(F("Timeout receiving array"));
-//    }
-//    return false;
-//  }
-}
-
-
-void redFlash(uint16_t flashes, uint16_t ondelaymS, uint16_t offdelaymS)
-{
-  uint16_t index;
-
-  pinMode(REDLED, OUTPUT);                    //setup pin as output
-
-  for (index = 1; index <= flashes; index++)
+  if (ARreceiveArrayL)
   {
-    digitalWrite(REDLED, LOW);
-    delay(ondelaymS);
-    digitalWrite(REDLED, HIGH);
-    delay(offdelaymS);
+    Monitorport.print(F("Received array length "));
+    Monitorport.println(ARreceiveArrayL);
+    return true;
   }
-  pinMode(REDLED, INPUT);                     //setup pin as input
+  else
+  {
+    Monitorport.println(F("Error receiving array"));
+    if (ARDTArrayTimeout)
+    {
+      Monitorport.println(F("Timeout receiving array"));
+    }
+    return false;
+  }
 }
 
+bool sendArray(uint8_t *array, uint32_t arrayL)
+{
+  char command[] = "CMD";
+  bool sentOK = false;
+
+  if (setupLoRaDevice())
+  {
+    Monitorport.print(F("Send with LoRa "));
+    Monitorport.println(command);
+  } 
+  else
+  {
+    Monitorport.println(F("LoRa device not available"));
+    return sentOK;
+  }
+
+  pinMode(REDLED, OUTPUT);
+  digitalWrite(REDLED,LOW);
+  sentOK = ARsendArrayNoACK(array, arrayL, command, strlen(command));
+  digitalWrite(REDLED,HIGH);
+  pinMode(REDLED, INPUT);  
+  SPI.end();
+  digitalWrite(NSS, HIGH);
+  digitalWrite(NRESET, HIGH);
+
+  if (sentOK)
+  {
+    Monitorport.print(command);
+    Monitorport.println(F(" Sent array OK"));
+  }
+  else
+  {
+    Monitorport.print(command);
+    Monitorport.println(F(" Send array failed"));
+  }
+  return sentOK;
+}
+
+uint32_t moveFileArray(char *filenamebuff, uint8_t *buff, uint32_t buffsize)
+{
+  uint32_t index;
+
+  ptrDTsendarray = buff;                                         //assign passed array ptr to global ptr
+
+  if (DTSD_initSD())
+  {
+#ifdef ENABLEMONITOR
+    Monitorport.println(F("SD Card initialized."));
+#endif
+  }
+  else
+  {
+    Monitorport.println(F("SD Card failed, or not present."));
+    while (1) redFlash(100, 25, 25);
+  }
+
+  DTLocalFileLength = DTSD_getFileSize(filenamebuff);            //get the file length
+
+  if (DTLocalFileLength == 0)
+  {
+    Monitorport.print(F("Error - opening local file "));
+    Monitorport.println(filenamebuff);
+    return 0;
+  }
+
+  if (DTLocalFileLength > buffsize)
+  {
+    Monitorport.println(filenamebuff);
+    Monitorport.print(F("Error - file length of "));
+    Monitorport.print(DTLocalFileLength);
+    Monitorport.print(F(" bytes exceeds array length of "));
+    Monitorport.print(buffsize);
+    Monitorport.println(F(" bytes"));
+    return 0;
+  }
+
+  DTSD_openFileRead(filenamebuff);
+#ifdef ENABLEMONITOR
+  Monitorport.print(F("Opened local file "));
+  Monitorport.print(filenamebuff);
+  Monitorport.print(F(" "));
+  Monitorport.print(DTLocalFileLength);
+  Monitorport.println(F(" bytes"));
+#endif
+  DTLocalFileCRC = DTSD_fileCRCCCITT(DTLocalFileLength);                   //get file CRC from position 0 to end
+#ifdef ENABLEMONITOR
+  Monitorport.print(F("DTLocalFileCRC 0x"));
+  Monitorport.println(DTLocalFileCRC, HEX);
+#endif
+
+  //now tranfer SD file to global array
+  dataFile.seek(0);                                                        //ensure at first position in file
+  for (index = 0; index < DTLocalFileLength; index++)
+  {
+    buff[index] = dataFile.read();
+  }
+
+  DTSD_closeFile();
+
+#ifdef ENABLEMONITOR
+  Monitorport.println(F("DTsendarray loaded from SD"));
+  Monitorport.print(F("Last written location "));
+  Monitorport.println(index);
+  Monitorport.print(F("First 16 bytes of array to send "));
+  LoRa.printHEXPacket(buff, 16);
+  Monitorport.println();
+#endif
+  DTarraylocation = 0;
+  return DTLocalFileLength;
+}
+
+bool takePhotoSend()
+{
+  bool sentOK = false;
+
+  camera_fb_t  * fb = esp_camera_fb_get();
+
+  if (!fb)
+  {
+    Monitorport.println(F("*****************************"));
+    Monitorport.println(F("ERROR - Camera capture failed"));
+    Monitorport.println(F("*****************************"));
+  } else {
+    Monitorport.println(F("Camera capture success"));
+  }
+
+
+  if (setupLoRaDevice())
+  {
+    Monitorport.print(F("Send Photo with LoRa"));
+  }
+  else
+  {
+    Monitorport.println(F("LoRa device not available"));
+    return sentOK;
+  }
+
+  pinMode(REDLED, OUTPUT);                    //setup pin as output 1
+  digitalWrite(REDLED, LOW);
+  sentOK = ARsendArray(fb->buf, fb->len, DTfilenamebuff, strlen(DTfilenamebuff)); //pass array pointer and length across to LoRa send function
+  digitalWrite(REDLED, HIGH);
+  pinMode(REDLED, INPUT);                    //setup pin as output 1
+
+  esp_camera_fb_return(fb);                                    //return the frame buffer back to the driver for reuse
+
+  SPI.end();
+  digitalWrite(NSS, HIGH);
+  digitalWrite(NRESET, HIGH);
+
+  if (sentOK)
+  {
+    Monitorport.print(DTfilenamebuff);
+    Monitorport.println(F(" Sent OK"));
+  }
+  else
+  {
+    Monitorport.print(DTfilenamebuff);
+    Monitorport.println(F(" Send picture failed"));
+  }
+  return sentOK;
+}
 
---
+++ Settings.h
@@ -0,0 +1,102 @@
+/*******************************************************************************************************
+  Programs for Arduino - Copyright of the author Stuart Robinson - 21/03/22
+
+  This program is supplied as is, it is up to the user of the program to decide if the program is
+  suitable for the intended purpose and free from errors.
+*******************************************************************************************************/
+
+#define SD SD_MMC
+
+//updated pinouts for 'ESP32CAM_Long_Range_Wireless_Adapter PCB dated 271121
+//Note transistor driving the White LED on pin 4, or the LED needs to be removed
+
+#define NSS 12                //select on LoRa device
+#define NRESET 15             //reset pin on LoRa device
+#define SCK 14                 //SCK on SPI3
+#define MISO 13               //MISO on SPI3 
+#define MOSI 2                //MOSI on SPI3
+#define DIO0 4                //DIO0 on SPI3
+//#define DIO0 16                //DIO0 on SPI3
+
+#define REDLED 33             //pin number for ESP32CAM on board red LED, set logic level low for on
+#define WHITELED 4             //pin number for ESP32CAM on board white LED
+
+#define LORA_DEVICE DEVICE_SX1278               //this is the device we are using
+
+#define Monitorport Serial                      //Serial port for prints 
+
+const uint32_t ACKtimeoutDTmS = 100;            //mS to wait for receiving an ACK and re-trying TX
+const uint32_t packetdelaymS = 0;               //mS delay between transmitted packets
+const uint32_t ReceiveTimeoutmS = 6000;        //mS waiting for array transfer before timeout
+
+//*******  Setup LoRa modem parameters here ! ***************
+const uint32_t Frequency = 434000000;           //frequency of transmissions in hertz
+const uint32_t Offset = 0;                      //offset frequency for calibration purposes
+
+const uint8_t Bandwidth = LORA_BW_500;          //LoRa bandwidth
+const uint8_t SpreadingFactor = LORA_SF7;       //LoRa spreading factor
+const uint8_t CodeRate = LORA_CR_4_5;           //LoRa coding rate
+const uint8_t Optimisation = LDRO_AUTO;         //low data rate optimisation setting, normally set to auto
+
+const int8_t TXpower = 10;                      //LoRa transmit power in dBm
+
+const uint32_t TXtimeoutmS = 5000;              //mS to wait for TX to complete
+const uint32_t RXtimeoutmS = 60000;             //mS to wait for receiving a packet
+const uint32_t ACKdelaymS = 25;                  //ms delay after packet actioned and ack sent
+const uint32_t ACKdelaystartendmS = 25;         //ms delay before ack sent at array start wrie and end write
+const uint32_t ACKsegtimeoutmS = 75;            //mS to wait for receiving an ACK before re-trying transmit segment
+const uint32_t ACKopentimeoutmS = 500;          //mS to wait for receiving an ACK before re-trying transmit file open
+const uint32_t ACKclosetimeoutmS = 500;         //mS to wait for receiving an ACK before re-trying transmit file close
+const uint32_t DuplicatedelaymS = 25;           //ms delay if there has been an duplicate segment or command receipt
+const uint32_t NoAckCountLimit = 250;           //if no NoAckCount exceeds this value - restart transfer
+
+const uint32_t FunctionDelaymS = 50;             //delay between functions such as open file, send segments etc
+const uint32_t PacketDelaymS = 1000;            //mS delay between transmitted packets such as DTInfo etc
+
+const uint8_t DTSegmentSize = 245;              //number of bytes in each segment or payload
+const uint8_t ARDTfilenamesize = 32;            //size of filename buffer
+
+const uint8_t StartAttempts = 2;                //number of attempts to start transfer before a fail
+const uint8_t SendAttempts = 5;                 //number of attempts carrying out a process before a restart
+
+const uint8_t HeaderSizeMax = 12;               //max size of header in bytes, minimum size is 7 bytes
+const uint8_t DataSizeMax = 245;                //max size of data array in bytes
+const uint16_t NetworkID = 0x3210;              //a unique identifier to go out with packet
+
+const uint16_t SleepTimesecs = 120;             //sleep time in seconds after each send loop
+const uint32_t uS_TO_S_FACTOR = 1000000;        //Conversion factor for micro seconds to seconds
+const uint8_t PicturesToTake = 1;               //number of pictures to take at each wakeup, only last is sent via LoRa
+const uint32_t PictureDelaymS = 1000;           //delay in mS between pictures
+const uint8_t SegmentSize = 245;                //number of bytes in each segment, 245 is maximum value for LoRa
+
+
+// Pin definition for CAMERA_MODEL_AI_THINKER
+// Change pin definition if you're using another ESP32 with camera module
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+uint8_t *PSRAMptr;      //create a global pointer to the array to send, so all functions have access
+uint32_t available_PSRAM;
+uint32_t allocated_PSRAM;                                                           
+
+RTC_DATA_ATTR int16_t bootCount = 0;             //variables to save in RTC ram
+RTC_DATA_ATTR uint16_t sleepcount = 0;
+
+camera_config_t config; //stores the camera configuration parameters
+
+uint32_t ARreceiveArrayL;
+
